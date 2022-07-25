6ae85fa70982a05de52d41c297ef8f272d29f4be
Stagav <73000023+onions127@users.noreply.github.com>
2022-07-25 23:13:34 +0300

    added home button to settings

--- app/src/main/java/com/example/itamargame/Settings.java
+++ app/src/main/java/com/example/itamargame/Settings.java
@@ -3,15 +3,19 @@ package com.example.itamargame;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.google.android.material.slider.Slider;
 
-public class Settings extends AppCompatActivity implements Slider.OnSliderTouchListener, Slider.OnChangeListener {
+public class Settings extends AppCompatActivity implements Slider.OnSliderTouchListener, Slider.OnChangeListener, View.OnClickListener {
 
     ImageView iv_speaker;
     Slider sound_slider;
+    Button btn_home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +24,11 @@ public class Settings extends AppCompatActivity implements Slider.OnSliderTouchL
 
         iv_speaker = findViewById(R.id.iv_speaker);
         sound_slider = findViewById(R.id.sound_slider);
+        btn_home = findViewById(R.id.btn_home);
 
         sound_slider.addOnSliderTouchListener(this);
         sound_slider.addOnChangeListener(this);
+        btn_home.setOnClickListener(this);
 
     }
 
@@ -48,5 +54,11 @@ public class Settings extends AppCompatActivity implements Slider.OnSliderTouchL
             iv_speaker.setImageResource(R.drawable.speaker);
         }
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent home = new Intent(Settings.this, MainActivity.class);
+        startActivity(home);
     }
 }
